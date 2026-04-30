83f641b923676b1bb3ee722c61af6728e0150a06
michaelborhlar <michealadefehinti09@gmail.com>
2026-04-30 17:47:59 +0100

    Add health check endpoint to urls.py

--- insighta_labs/urls.py
+++ insighta_labs/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+    path("health", health),
     path("auth/", include("authentication.urls")),
     path("api/", include("profiles.urls")),
 ]
