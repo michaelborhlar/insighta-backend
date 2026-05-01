6bc19e0fba046b7696288feb7957228ab2e17a09
michaelborhlar <michealadefehinti09@gmail.com>
2026-05-01 14:44:11 +0100

    Remove leading slashes from URL paths

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from authentication import views
 
 urlpatterns = [
-    path("/github", views.github_authorize, name="github-authorize"),
-    path("/github/callback", views.github_callback, name="github-callback"),
-    path("/refresh", views.refresh_token_view, name="token-refresh"),
-    path("/logout", views.logout_view, name="logout"),
-    path("/whoami", views.whoami_view, name="whoami"),
+    path("github", views.github_authorize, name="github-authorize"),
+    path("github/callback", views.github_callback, name="github-callback"),
+    path("refresh", views.refresh_token_view, name="token-refresh"),
+    path("logout", views.logout_view, name="logout"),
+    path("whoami", views.whoami_view, name="whoami"),
 ]
